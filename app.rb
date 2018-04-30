7037041a8757d1416436a57dec2f5c7387512c0f
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-04-30 15:52:16 -0400

    make it even better

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require "sinatra"
 require "sinatra/activerecord"
-require_relative './models/user'
-require_relative './models/post'
-require_relative './models/tag'
-require_relative './models/postTag'
+require_relative 'models/user'
+require_relative 'models/post'
+require_relative 'models/tag'
+require_relative 'models/postTag'
 
 set :database, {adapter: 'postgresql', database: 'icelandblog'}
 enable :sessions
