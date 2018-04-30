0e56a35fcf4d5dd7af5d4a6da5e7add22b9a037e
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-04-30 16:19:52 -0400

    make it even better

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require "sinatra"
 require "sinatra/activerecord"
-require_relative './models/user'
+require_relative 'models/user'
 require_relative './models/post'
 require_relative './models/tag'
 require_relative './models/postTag'
